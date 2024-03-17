22e8e0814d4e1e3228c8d4d3d210c8178ddca073
BasilSteel <81254874+BasilSteel@users.noreply.github.com>
2024-03-17 14:18:34 +0500

    tqweqw

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,83 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { RouterProvider, createBrowserRouter, Navigate } from 'react-router-dom'
-import Reg from './reg/Reg'
-import Log from './auth/Auth'
-import { useSelector } from 'react-redux'
-import MainPage from './MainPage'
-import Workspace from './workspace'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Navigate,
+} from "react-router-dom";
+import Reg from "./reg/Reg";
+import Log from "./auth/Auth";
+import { useSelector } from "react-redux";
+import MainPage from "./MainPage";
+import Workspace from "./workspace";
+import NewModalAddCard  from "./NewModalAddCard";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Navigate to="/reg" />
+    path: "/",
+    element: <Navigate to="/reg" />,
   },
   {
-    path: '/reg',
-    element: <Reg />
+    path: "/reg",
+    element: <Reg />,
   },
   {
-    path: '/auth',
-    element: <Log />
+    path: "/auth",
+    element: <Log />,
   },
-])
+]);
 
 const authRouter = createBrowserRouter([
   {
-    path: '/',
-    element: <MainPage />
+    path: "/",
+    element: <MainPage />,
   },
   {
-    path: '/reg',
-    element: <Navigate to="/" />
+    path: "/reg",
+    element: <Navigate to="/" />,
   },
   {
-    path: '/auth',
-    element: <Navigate to="/" />
+    path: "/auth",
+    element: <Navigate to="/" />,
   },
-])
+]);
 
 const authRouterAdmin = createBrowserRouter([
   {
-    path: '/',
-    element: <MainPage />
+    path: "/",
+    element: <MainPage />,
   },
   {
-    path: '/reg',
-    element: <Navigate to="/" />
+    path: "/reg",
+    element: <Navigate to="/" />,
   },
   {
-    path: '/auth',
-    element: <Navigate to="/" />
+    path: "/auth",
+    element: <Navigate to="/" />,
   },
   {
-    path: 'admin',
-    element: <>admin</>
+    path: "admin",
+    element: <>admin</>,
   },
   {
-    path: '/workspace',
-    element: <Workspace />
+    path: "/workspace",
+    element: <Workspace />,
   },
   {
-    path: '/MainPage',
-    element: <MainPage />
+    path: "/MainPage",
+    element: <MainPage />,
   },
-])
+  {
+    path: "/test",
+    element: <NewModalAddCard  />,
+  },
+]);
 
 function App() {
-
-  const token = useSelector((state) => state.auth.token)
-  const role = useSelector((state) => state.auth.role)
+  const token = useSelector((state) => state.auth.token);
+  const role = useSelector((state) => state.auth.role);
 
   console.log(token);
 
-
-  return (
-    token ? role === "ADMIN" ? <RouterProvider router={authRouterAdmin} /> : <RouterProvider router={authRouterAdmin} /> :
+  return token ? (
+    role === "ADMIN" ? (
+      <RouterProvider router={authRouterAdmin} />
+    ) : (
+      <RouterProvider router={authRouterAdmin} />
+    )
+  ) : (
     <RouterProvider router={router} />
-  )
+  );
 }
 
-export default App
+export default App;
